779bb64d1862a6e9042a1d344eafecdd02a58a66
ffdrko <faisal.deepto@gmail.com>
2026-01-06 15:43:07 +0600

    Code error

---
+++ 1_Jan_26/Code_Experiment/c1d_0.py
@@ -0,0 +1,12 @@
+# Code Error
+user_prompt = "Enter todo: "
+todo_1 = input(user_prompt)
+todo_2 = input(user_prompt)
+todo_3 = input(user_prompt)
+
+todo_list = [todo_1, todo_2, todo_3, "Hello"]
+
+print(todo_list)
+
+print(type(todo_list))
+print(type(todo_1))
