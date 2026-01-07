379d9e9478f76f2977ca505a23fe6bfaf91a8fe1
ffdrko <faisal.deepto@gmail.com>
2026-01-07 12:43:20 +0600

    Password checker

---
+++ 1_Jan_26/Day-2/Bonus/b2_0.py
@@ -0,0 +1,7 @@
+password = input("Enter your password: ")
+
+while password != "ffd123":
+    print("Incorrect password. Please try again.")
+    password = input("Enter your password: ")
+
+print("Access granted.")
