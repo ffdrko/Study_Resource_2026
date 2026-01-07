ee7572aadcc183941aaf88ea3dfc85cc2bd6ba42
ffdrko <faisal.deepto@gmail.com>
2026-01-07 13:18:58 +0600

    Day-2 bug fix

---
+++ 1_Jan_26/Day-2/Bug/b2g.py
@@ -0,0 +1,13 @@
+# countries = []
+
+# while True:
+#     country = input("Enter a country name: ")
+#     countries.append(country)
+# print("Current list of countries:", countries)
+
+countries = []
+
+while True:
+    country = input("Enter a country name: ")
+    countries.append(country)
+    print("Current list of countries:", countries)
