9dc8ee1a6bc0c5b05424e40137a43c3fe0318702
ffdrko <faisal.deepto@gmail.com>
2026-01-07 11:33:05 +0600

    Day-1 recap on Day-2

---
+++ 1_Jan_26/Day-2/d2s_0.py
@@ -0,0 +1,9 @@
+user_prompt = "Enter a todo: "
+
+todo_1 = input(user_prompt)
+todo_2 = input(user_prompt)
+todo_3 = input(user_prompt)
+
+todo_list = [todo_1, todo_2, todo_3]
+print("Your todo list:")
+print(todo_list)
