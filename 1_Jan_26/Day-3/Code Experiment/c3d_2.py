237e5be425fe2d9aa3e2f318d22db9f28d3d25a8
ffdrko <faisal.deepto@gmail.com>
2026-01-08 11:36:59 +0600

    Day-3 code experiment

---
+++ 1_Jan_26/Day-3/Code Experiment/c3d_2.py
@@ -0,0 +1,19 @@
+# Match case
+todo = []
+
+while True:
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo_item = input("Enter a to-do item: ")
+            todo.append(todo_item)
+        case "show":
+            for item in todo:
+                item = item.title()
+                print(item)
+        case "exit":
+            break
+        case _:
+            print("Invalid command. Please try again.")
