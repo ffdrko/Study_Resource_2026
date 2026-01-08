e265e3fcadeb8c7c4ed00d8cffbe3535813c0105
ffdrko <faisal.deepto@gmail.com>
2026-01-08 11:31:44 +0600

    Day-3 code experiment

---
+++ 1_Jan_26/Day-3/Code Experiment/c3d_1.py
@@ -0,0 +1,18 @@
+# Bitwise OR operator
+todo = []
+
+while True:
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case "add":
+            todo_item = input("Enter a to-do item: ")
+            todo.append(todo_item)
+        case "show" | "display":
+            for item in todo:
+                print(item)
+        case "exit":
+            break
+        case _:
+            print("Invalid command. Please try again.")
