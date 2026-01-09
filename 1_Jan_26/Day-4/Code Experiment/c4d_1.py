fd172d91ecd3983b86a0793f3c0e1d0717418ca3
ffdrko <faisal.deepto@gmail.com>
2026-01-09 11:26:57 +0600

    Day 4 Code experiment

---
+++ 1_Jan_26/Day-4/Code Experiment/c4d_1.py
@@ -0,0 +1,9 @@
+# Importaance of types
+
+a = 'hi'
+print(type(a))  # This will print: <class 'str'>
+
+a = int(a)
+print(type(a))  # This will print: <class 'int'>
+
+# all str can not be converted to int
