888b897567f8d702959d5d2cf4cd18b3099bdd5b
ffdrko <faisal.deepto@gmail.com>
2026-01-09 11:32:17 +0600

    Day 4 Code experiment

---
+++ 1_Jan_26/Day-4/Code Experiment/c4d_2.py
@@ -0,0 +1,11 @@
+# List Indexing
+
+mylist = ['a', 'b', 'c', 'd', 'e']
+
+z = mylist[2]
+print(z)  # This will print: c
+print(type(z))  # This will print: <class 'str'>
+print(type(mylist))  # This will print: <class 'list'>
+
+print(mylist.index('a'))  # This will print: 0
+print(dir(mylist))  # This will print all the methods available for list objects
