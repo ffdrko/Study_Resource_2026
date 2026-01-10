2ed425c21581db11335cdf3378f3c1dec868c2ab
ffdrko <faisal.deepto@gmail.com>
2026-01-10 11:14:47 +0600

    Day-5 enumerate funtion

---
+++ 1_Jan_26/Day-5/d5s_1.py
@@ -0,0 +1,19 @@
+todo_list = []
+
+while True:
+    user_action = input("Type 'add', 'show', 'edit', or 'exit': ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todo_list.append(todo)
+        case 'show':
+            for index, item in enumerate(todo_list):
+                print(index, '-', item)
+        case 'edit':
+            number = int(input("Enter the number of the todo to edit: "))
+            new_todo = input("Enter the new todo: ")
+            todo_list[number - 1] = new_todo
+        case 'exit':
+            break
