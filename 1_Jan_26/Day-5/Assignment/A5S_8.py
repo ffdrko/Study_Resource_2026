7de04ac4fcf6094602cddadcb3f25fa5f1b2760a
ffdrko <faisal.deepto@gmail.com>
2026-01-10 12:24:42 +0600

    Day-5 Assignment 9

---
+++ 1_Jan_26/Day-5/Assignment/A5S_8.py
@@ -0,0 +1,10 @@
+# We have defined a list in the coding area. Add some code that:
+
+# (1) iterates over the items of mylist,
+
+# (2) prints out the current length of the list in each iteration
+
+mylist = ['a', 'b', 'c', 'd']
+
+for item in mylist:
+    print(f"Current item is: {item} length of the list is : {len(mylist)}")
