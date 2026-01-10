4a3d79be72ff8714c0a17dd6e321b4c70705340c
ffdrko <faisal.deepto@gmail.com>
2026-01-10 12:35:44 +0600

    Day-5 bug fixed

---
+++ 1_Jan_26/Day-5/Bug/b5g_0.py
@@ -0,0 +1,18 @@
+# Bug-Fixing Exercise 1
+# Supposedly, the following program should:
+
+# 1. Prompt the user to input an index (e.g., 0, 1, or 2).
+
+# 2. Print out the item with that index.
+
+# However, there is a bug with the program which you should try to fix.
+
+
+
+# menu = ["pasta", "pizza", "salad"]
+
+# user_choice = input("Enter the index of the item: ")
+
+menu = ["pasta", "pizza", "salad"]
+
+user_choice = int(input("Enter the index of the item: "))
