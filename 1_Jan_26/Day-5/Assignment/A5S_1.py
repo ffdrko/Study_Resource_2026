8497e66a96f53f725ca8f6530122181c1819696b
ffdrko <faisal.deepto@gmail.com>
2026-01-10 11:18:19 +0600

    Day-5 Assignment 2

---
+++ 1_Jan_26/Day-5/Assignment/A5S_1.py
@@ -0,0 +1,9 @@
+# Your task for this exercise is to
+
+# (1) assign a list to the rainfallvariable.
+
+# (2) The list should contain four items, a float, an integer, a string, and a list*. 
+
+# *It's totally fine for a list to contain another list as an item.
+
+rainfall = [32.5, 40, "moderate", [12,15,20]]
