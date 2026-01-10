6bbc48adf6902dbd7fb15adc89fc0e82fdc609f3
ffdrko <faisal.deepto@gmail.com>
2026-01-10 12:19:42 +0600

    Day-5 Assignment 8

---
+++ 1_Jan_26/Day-5/Assignment/A5S_7.py
@@ -0,0 +1,15 @@
+# Your task for this exercise is to:
+
+# (1) assign a string to mystring variable,
+
+# (2) assign a list to mylist variable,
+
+# (3) print out the length of mystring
+
+# (4) print out the length of mylist
+
+mystring = "Hello, world!"
+print(len(mystring))
+
+mylist = [1, 2, 3, 4]
+print(len(mylist))
