41f20ac373a1be8778fd6fd12cf6d7262c31b40b
ffdrko <faisal.deepto@gmail.com>
2026-01-10 11:16:49 +0600

    Day-5 Assignment 1

---
+++ 1_Jan_26/Day-5/Assignment/A5S_0.py
@@ -0,0 +1,7 @@
+# Your task for this exercise is to:
+
+# (1) assign a list to the variable temperatures.
+
+# (2) The list should contain three items, a float, an integer, and a string.
+
+temperatures = [98.55, 36, "hot"]
