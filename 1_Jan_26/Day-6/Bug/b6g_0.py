e09dfe8c3fa785bfedd47524346b37c508ad71af
ffdrko <faisal.deepto@gmail.com>
2026-01-11 13:51:22 +0600

    Day-6 Bug Fixed

--- 1_Jan_26/Day-6/Bug/b6g_0.py
+++ 1_Jan_26/Day-6/Bug/b6g_0.py
@@ -1,3 +1,7 @@
+# file = open("1_Jan_26/Day-6/File/data.txt", "w")
+# file.write("100.12")
+# file.write("200.23")
+
 file = open("1_Jan_26/Day-6/File/data.txt", "w")
 file.write("100.12\n")
 file.write("200.23\n")
---
+++ 1_Jan_26/Day-6/Bug/b6g_1.py
@@ -0,0 +1,7 @@
+# file = open("1_Jan_26/Day-6/File/data.txt", "r")
+# file.write("100.12\n")
+# file.close()
+
+file = open("1_Jan_26/Day-6/File/data.txt", "w")
+file.write("100.12\n")
+file.close()
