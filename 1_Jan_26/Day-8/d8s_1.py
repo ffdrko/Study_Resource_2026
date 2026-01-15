9d572ab64f6f06acbb6fa481691b7b9b9aef51a0
ffdrko <faisal.deepto@gmail.com>
2026-01-15 10:20:47 +0600

    Day-8 file optimize by using with

---
+++ 1_Jan_26/Day-8/d8s_1.py
@@ -0,0 +1,32 @@
+while True:
+    user_action = input("Type 'add', 'show', 'edit', 'complete' or 'exit': ")
+
+    match user_action:
+        case 'add':
+            todo_item = input("Enter a todo item: ")
+
+            with open("1_Jan_26/Day-8/file/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+
+
+            todo_list.append(todo_item + "\n")
+
+            with open("1_Jan_26/Day-8/file/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        case 'show':
+            with open("1_Jan_26/Day-8/file/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+
+            for index, item in enumerate(todo_list):
+                item = item.strip("\n")
+                print(f"{index + 1}-{item}")
+        case 'edit':
+            todo_num = int(input("Enter the number of the todo item to edit: "))
+            new_todo = input("Enter a new todo item: ")
+
+            todo_list[todo_num - 1] = new_todo + "\n"
+        case 'complete':
+            todo_num = int(input("Enter the number of the todo item to edit: "))
+            todo_list.pop(todo_num - 1)
+        case 'exit':
+            break
