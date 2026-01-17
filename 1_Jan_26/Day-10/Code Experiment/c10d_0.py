84abb3731b0ed7a0c45b47f9f29291b7fac5b261
ffdrko <faisal.deepto@gmail.com>
2026-01-17 18:48:30 +0600

    Day-10 code experiment

---
+++ 1_Jan_26/Day-10/Code Experiment/c10d_0.py
@@ -0,0 +1,56 @@
+# there are two types of error one is syntax error and other one is exception error
+FILE_PATH = "./Files/todo_list.txt"
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+
+    if user_action.startswith('add'):
+        todo_item = user_action[4:] + '\n'
+
+        with open(FILE_PATH) as file:
+            todo_list = file.readlines()
+
+        todo_list.append(todo_item)
+
+        with open(FILE_PATH, 'w') as file:
+            file.writelines(todo_list)
+
+    elif user_action.startswith('show'):
+        with open(FILE_PATH) as file:
+            todo_list = file.readlines()
+
+        for index, item in enumerate(todo_list):
+            item = item.strip("\n")
+            print(f"{index+1}-{item}")
+    elif user_action.startswith('edit'):
+        try:
+            todo_item = int(user_action[5:])
+            with open(FILE_PATH) as file:
+                todo_list = file.readlines()
+
+            todo_list[todo_item-1] = input("Enter new item to add: ") + '\n'
+
+            with open(FILE_PATH, 'w') as file:
+                file.writelines(todo_list)
+        except ValueError:
+            print("use item number after edit.")
+    elif user_action.startswith('complete'):
+        try:
+            todo_num = int(user_action[9:]) - 1
+
+            with open(FILE_PATH) as file:
+                todo_list = file.readlines()
+
+            todo_list.pop(todo_num)
+
+            with open(FILE_PATH, "w") as file:
+                file.writelines(todo_list)
+        except IndexError:
+            with open(FILE_PATH) as file:
+                todo_list = file.readlines()
+
+            print(f"The length of todo_list is {len(todo_list)}")
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Invalid action")
