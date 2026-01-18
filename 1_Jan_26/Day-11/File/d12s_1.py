b1dde0ca30c9888c30ddce968965cde6b136b805
ffdrko <faisal.deepto@gmail.com>
2026-01-18 12:48:56 +0600

    Day-11 customs function implementation

---
+++ 1_Jan_26/Day-11/File/d12s_1.py
@@ -0,0 +1,58 @@
+FILE_PATH = '1_Jan_26/Day-11/File/todo_list.txt'
+
+def get_todo():
+    with open(FILE_PATH) as file:
+        todo_list_local = file.readlines()
+    return todo_list_local
+
+
+while True:
+    user_action = input("Type add, show, edit, complete and exit: ")
+
+    if user_action.startswith('add'):
+        todo_item = user_action[4:] + "\n"
+
+        todo_list = get_todo()
+
+        todo_list.append(todo_item)
+
+        with open(FILE_PATH, 'w') as file:
+            file.writelines(todo_list)
+
+    elif user_action.startswith('show'):
+        todo_list = get_todo()
+        
+        for index, item in enumerate(todo_list):
+            item = item.strip('\n')
+            print(f"{index + 1}-{item}")
+    
+    elif user_action.startswith('edit'):
+        try:
+            item_num = int(user_action[5:])
+            todo_list = get_todo()
+            
+            todo_list[item_num - 1] = input("Enter your new todo item: ") + "\n"
+
+            with open(FILE_PATH, 'w') as file:
+                file.writelines(todo_list)
+        
+        except ValueError:
+            print("Afer edit you have enter number")
+    
+    elif user_action.startswith("complete"):
+        try:
+            item_num = int(user_action[9:])
+            todo_list = get_todo()
+            
+            todo_list.pop(item_num - 1)
+
+            with open(FILE_PATH, 'w') as file:
+                file.writelines(todo_list)
+        
+        except IndexError:
+            print("The number is not inside the list.")
+    elif user_action.startswith('exit'):
+        break
+    
+    else:
+        print('invalid comment')
