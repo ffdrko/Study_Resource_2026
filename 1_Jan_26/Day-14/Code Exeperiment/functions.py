875685597cb66d0bd45aea99a63f152460c547c2
ffdrko <faisal.deepto@gmail.com>
2026-01-22 15:55:02 +0600

    Day-14 Code Experiment

--- 1_Jan_26/Day-14/Code Exeperiment/functions.py
+++ 1_Jan_26/Day-14/Code Exeperiment/functions.py
@@ -10,4 +10,6 @@ def write_todo(todo_list_arg, filepath="Day-14/File/todo_list.txt"):
     with open(filepath, "w") as file:
         file.writelines(todo_list_arg)
 
-print("Hello from functions")
+
+if __name__ == "__main__":
+    print("This is a module for todo list functions.")
