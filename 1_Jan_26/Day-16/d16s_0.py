3f59eceb4b1842c35806d1c1fa0015590c755763
ffdrko <faisal.deepto@gmail.com>
2026-01-25 12:56:25 +0600

    Day-15 recap.

---
+++ 1_Jan_26/Day-16/d16s_0.py
@@ -0,0 +1,15 @@
+FILEPATH = 'Day-16/File/todo_list.txt'
+
+def read_todo(filepath = FILEPATH):
+    with open(filepath) as file:
+        local_list = file.readlines()
+    return local_list
+
+
+def write_todo(list_args, filepath = FILEPATH):
+    with open(filepath) as file:
+        file.writelines(list_args)
+
+
+if __name__ == "__main__":
+    print('This file contains all the functions.')
