f4b3b6c6956c8696a9aba46f0c4f43cd577b5232
ffdrko <faisal.deepto@gmail.com>
2026-01-25 19:15:36 +0600

    adding lable and input box

--- 1_Jan_26/Day-16/d16s_2.py
+++ 1_Jan_26/Day-16/d16s_2.py
@@ -1,7 +1,9 @@
 import d16s_0 as func
 import FreeSimpleGUI as sg
 
+label = sg.Text("Type in a to-do item: ")
+input_box = sg.InputText(tooltip= "Enter todo")
 
-window = sg.Window("My To-Do App", layout=[""])
+window = sg.Window("My To-Do App", layout=[[label, input_box]])
 window.read()
 window.close()
