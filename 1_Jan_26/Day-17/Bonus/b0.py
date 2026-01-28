fc8a3f1e68da182acca9bd3e6412a2ae574bd4f1
ffdrko <faisal.deepto@gmail.com>
2026-01-28 22:32:27 +0600

    Day-17 File Compressor file browse and folder browse added

--- 1_Jan_26/Day-17/Bonus/b0.py
+++ 1_Jan_26/Day-17/Bonus/b0.py
@@ -2,16 +2,19 @@ import FreeSimpleGUI as Sg
 
 label_1 = Sg.Text("Select files to compress: ")
 input_1 = Sg.Input()
-choose_btn_1 = Sg.Button("Choose")
+choose_btn_1 = Sg.FileBrowse("Choose")
 
 label_2 = Sg.Text("Select files to compress: ")
 input_2 = Sg.Input()
-choose_btn_2 = Sg.Button("Choose")
+choose_btn_2 = Sg.FolderBrowse("Choose")
 
 compress_btn = Sg.Button("Compress")
 window = Sg.Window("File compressor", layout=[[label_1,input_1,
                                                choose_btn_1],
                                               [label_2,input_2,choose_btn_2]
                                               ,[compress_btn]])
-window.read()
+while True:
+    event, value = window.read()
+    print(event, value)
+
 window.close()
