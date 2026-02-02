60f4427d0f3e9fca520695fe6793886b8202ec07
ffdrko <faisal.deepto@gmail.com>
2026-02-02 21:44:06 +0600

    Assignment 1 another version

--- 2_feb_26/Part_1/main_4.py
+++ 2_feb_26/Part_1/main_4.py
@@ -1,4 +1,9 @@
-print("Your Learing Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+# Assignment 1
+
+print("Your Learing Path:\n" \
+"\t- Python Basics\n" \
+"\t- Data Engineering\n" \
+"\t- AI")
 # print("\t- Python Basics")
 # print("\t- Data Engineering")
 # print("\t- AI")
