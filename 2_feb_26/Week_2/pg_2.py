567cbbb388c655addcf36bcd8114922e77b6bd75
ffdrko <faisal.deepto@gmail.com>
2026-02-16 22:55:35 +0600

    Dino output using print.

--- 2_feb_26/Week_2/pg_2.py
+++ 2_feb_26/Week_2/pg_2.py
@@ -1,3 +1,13 @@
 print("Hi python")
 print('Hi python')
 # print("Hi python')
+
+print("----------------")
+print("  LEARN PYTHON  ")
+print("----------------")
+
+print("            __")
+print("           / _)")
+print("    .-^^^-/ /")
+print(" __/       /")
+print("<__.|_|-|_|")
