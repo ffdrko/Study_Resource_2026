cc8c748f1763495d0bd15f008cbdda580720c598
ffdrko <faisal.deepto@gmail.com>
2026-02-17 22:43:26 +0600

    Assignment 2.

---
+++ 2_feb_26/Week_2/pg_6.py
@@ -0,0 +1,8 @@
+info_email = "info@datawithbaraa.com"
+support_email = "support@datawithbaraa.com"
+
+web_url = "www.datawithbaraa.com"
+
+print("For more information, please contact us at", info_email)
+print("For support, please contact us at", support_email)
+print("Visit our website at", web_url)
