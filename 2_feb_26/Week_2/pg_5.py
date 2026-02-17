6a670416e8f8fb0b7873f6781433cd7aa99be691
ffdrko <faisal.deepto@gmail.com>
2026-02-17 22:40:12 +0600

    use of variable.

--- 2_feb_26/Week_2/pg_5.py
+++ 2_feb_26/Week_2/pg_5.py
@@ -1,10 +1,11 @@
 name = "Bara"
+language = "Python"
 print("My name is", name)
-print(name,"is learning Python")
-print(name,"wants to become python expert")
+print(name,"is learning", language)
+print(name,"wants to become", language,"expert")
 
 name = "Maria"
 
 print("My name is", name)
-print(name,"is learning Python")
-print(name,"wants to become python expert")
+print(name,"is learning", language)
+print(name,"wants to become", language,"expert")
