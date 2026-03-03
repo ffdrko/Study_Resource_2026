10e269358186c10218015bd10edeab67249bf4c3
ffdrko <faisal.deepto@gmail.com>
2026-03-03 19:04:05 +0600

    Part_1 exercise 6 done.

---
+++ 3_mar_26/Part_1/pg_9.py
@@ -0,0 +1,11 @@
+"""
+problem-6
+1) Create list of three strings. The strings can be any text (e.g., "Good Morning", "Dog", etc.)
+
+(2) associate the list with a variable named words,
+
+(3) print out the variable/list.
+"""
+
+words = ["Hello", "Hi", "Bye"]
+print(words)
