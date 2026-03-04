7f297585991db5a84d437c1d93ec9f501af15ed0
ffdrko <faisal.deepto@gmail.com>
2026-03-04 23:28:42 +0600

    Part_1 exercise-14 done

---
+++ 3_mar_26/Part_1/pg_5.py
@@ -0,0 +1,18 @@
+"""
+problem-14
+Create a program that:
+
+(1) prompts the user to input their name,
+
+(2) prints out the name with the first letter capitalized,
+
+(3) keeps prompting the user to input another name
+
+(4)  prints out the name with the first letter capitalized,
+
+(5) The process is repeated infinitely.
+"""
+
+while True:
+    user_name = input("What is your name?")
+    print(user_name)
