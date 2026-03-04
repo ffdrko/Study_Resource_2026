204a88910769e853af45970ed7f6ed8a1f72e655
ffdrko <faisal.deepto@gmail.com>
2026-03-04 22:58:44 +0600

    Part_1 Code Experiment-2

--- 3_mar_26/Part_1/spg_9.py
+++ 3_mar_26/Part_1/spg_9.py
@@ -10,10 +10,22 @@ Experiment-1 ( Infinite while-loop)
 Experiment-2 ( Inside vs outside the loop)
 """
 
+# todo_list = []
+#
+# while True:
+#     user_prompt = "Enter a todo: "
+#     todo_item = input(user_prompt)
+#     print(todo_item.capitalize())
+#     todo_list.append(todo_item)
+
+"""
+Experiment-3 (Capitalize vs Title)
+"""
+
 todo_list = []
 
 while True:
     user_prompt = "Enter a todo: "
     todo_item = input(user_prompt)
-    print(todo_item.capitalize())
+    print(todo_item.title())
     todo_list.append(todo_item)
