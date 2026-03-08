309a08945003b7e03c06b5818fcc1a26535e8201
Fahim Faisal Deepto <56530414+ffdrko@users.noreply.github.com>
2026-03-08 14:26:03 +0600

    Add files via upload
    rust programing testing

---
+++ 3_mar_26/rust_project/target/debug/deps/rust_project-311b847d8eb0082c.d
@@ -0,0 +1,5 @@
+E:\rust_project\target\debug\deps\rust_project-311b847d8eb0082c.d: src\main.rs
+
+E:\rust_project\target\debug\deps\librust_project-311b847d8eb0082c.rmeta: src\main.rs
+
+src\main.rs:
