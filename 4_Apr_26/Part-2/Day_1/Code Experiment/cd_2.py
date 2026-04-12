898f6df6df82f1cab06cd25ceb5772ddd1fa8674
ffdrko <faisal.deepto@gmail.com>
2026-04-12 14:37:41 +0600

    Day_1 code experiment 3

---
+++ 4_Apr_26/Part-2/Day_1/Code Experiment/cd_2.py
@@ -0,0 +1,13 @@
+# Experiment-3: ide error hints
+user_prompt = 'Enter a todo: '
+
+user_todo_1 = input(user_prompt)
+user_todo_2 = input(user_prompt)
+user_todo_3 = input(user_prompt)
+
+todo_list = [user_todo_1, user_todo_2, user_todo_3]
+
+print(todo_list)
+
+print(type(todo_list))
+print(type(user_todo_1))
