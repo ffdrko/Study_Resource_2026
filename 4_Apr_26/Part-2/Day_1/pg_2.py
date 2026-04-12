dbba9e37b487a92258b45bca82d3dfe2095e95da
ffdrko <faisal.deepto@gmail.com>
2026-04-12 14:25:53 +0600

    Day_1 use list to store many value at a time.

---
+++ 4_Apr_26/Part-2/Day_1/pg_2.py
@@ -0,0 +1,9 @@
+user_prompt = "Enter a todo: "
+
+user_todo_1 = input(user_prompt)
+user_todo_2 = input(user_prompt)
+user_todo_3 = input(user_prompt)
+
+todo_list = [user_todo_1, user_todo_2, user_todo_3]
+
+print(todo_list)
