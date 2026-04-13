ec7ea45c07a591afcef643899df9fd658086932c
ffdrko <faisal.deepto@gmail.com>
2026-04-13 10:50:13 +0600

    Day_2 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_2/Code Experiment/cd_2.py
@@ -0,0 +1,10 @@
+# Experiment - 3 capitalize() vs title()
+user_prompt = "Enter a todo: "
+todo_list = []
+
+while True:
+    user_todo = input(user_prompt)
+    print(user_todo.title())
+    print(user_todo.capitalize())
+    todo_list.append(user_todo)
+    print(todo_list)
