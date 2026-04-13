5946fd314ed6b10c468f77f8c76a7f7fc93f7081
ffdrko <faisal.deepto@gmail.com>
2026-04-13 11:07:41 +0600

    Day_2 Bug fixed

---
+++ 4_Apr_26/Part-2/Day_2/Bug/bg_3.py
@@ -0,0 +1,13 @@
+# countries = []
+
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+# print(countries)
+
+countries = []
+
+while True:
+    country = input("Enter the country: ")
+    countries.append(country)
+    print(countries)
