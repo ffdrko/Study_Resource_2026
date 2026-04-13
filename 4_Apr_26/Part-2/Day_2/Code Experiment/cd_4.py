1ab0059446b6afc256d62f505fead8ab9c2e31af
ffdrko <faisal.deepto@gmail.com>
2026-04-13 10:52:12 +0600

    Day_2 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_2/Code Experiment/cd_4.py
@@ -0,0 +1,10 @@
+# Experiment - 5 no paraenthesis, no output
+user_prompt = "Enter a todo: "
+todo_list = []
+
+while True:
+    user_todo = input(user_prompt)
+    print(user_todo.title())
+    print(user_todo.capitalize())
+    todo_list.append(user_todo)
+    print(todo_list)
