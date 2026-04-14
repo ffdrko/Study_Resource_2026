0e6f97552b5ba7bda837d8922a2a1d426a4978ab
ffdrko <faisal.deepto@gmail.com>
2026-04-14 12:48:58 +0600

    Day_3 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_3/Code Experiment/cd_0.py
@@ -0,0 +1,20 @@
+# Experiment - 1 Match-case 
+
+todo_list = []
+
+while True:
+    user_action = input("Enter add, show oor exit: ")
+
+    match user_action:
+        case "add":
+            user_todo = input("Enter a todo: ")
+            todo_list.append(user_todo)
+        case "show":
+            for i in todo_list:
+                print(i)
+        case "exit":
+            break
+        case _:
+            print("Please enter a valid command")
+
+print("Bye!")
