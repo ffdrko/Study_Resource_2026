411611d1bd28fe008af582409def08b3b02b5f7d
ffdrko <faisal.deepto@gmail.com>
2026-04-14 14:11:43 +0600

    Day_3 bug fixed

---
+++ 4_Apr_26/Part-2/Day_3/Bug/bg_0.py
@@ -0,0 +1,9 @@
+# for i in buttons:
+#     print(i.capitalize())
+
+# buttons = ["cancel", "reply", "submit"]
+
+buttons = ["cancel", "reply", "submit"]
+
+for i in buttons:
+    print(i.capitalize())
