0702d02a70caae7833ee75f111bf85037337c64a
ffdrko <faisal.deepto@gmail.com>
2026-04-15 13:18:09 +0600

    Day_4 Bonus(manipulare the string)

--- 4_Apr_26/Part-2/Day_4/Bonus/bn_0.py
+++ 4_Apr_26/Part-2/Day_4/Bonus/bn_0.py
@@ -1,4 +1,7 @@
 filesnames = ["1.Raw Data.txt", "2.Cleaned Data.txt", "3.Analysis Results.txt"]
 
 for file in filesnames:
-    print(file.replace(".", "_", count=1))
+    file = file.replace(" ", "_", 1)
+    print(file)
+
+print(filesnames)
