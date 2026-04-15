2bbf42ac963ebe82fa7fec40cbc2694c380b764e
ffdrko <faisal.deepto@gmail.com>
2026-04-15 13:22:23 +0600

    Day_4 Bonus(manipulare the string) using tuple

--- 4_Apr_26/Part-2/Day_4/Bonus/bn_0.py
+++ 4_Apr_26/Part-2/Day_4/Bonus/bn_0.py
@@ -1,7 +1,10 @@
 filesnames = ["1.Raw Data.txt", "2.Cleaned Data.txt", "3.Analysis Results.txt"]
+new_list = []
 
 for file in filesnames:
     file = file.replace(" ", "_", 1)
+    new_list.append(file)
     print(file)
 
 print(filesnames)
+print(new_list)
