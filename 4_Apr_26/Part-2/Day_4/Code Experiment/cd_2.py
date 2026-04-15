73e8d131a5ea0c34d040ead71e9cea231ada1569
ffdrko <faisal.deepto@gmail.com>
2026-04-15 13:09:26 +0600

    Day_4 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_4/Code Experiment/cd_2.py
@@ -0,0 +1,10 @@
+# Experiment - 3 List indexing
+
+mylist = ['a', 'b', 'c', 'd', 'e']
+
+print(mylist[0])  # Output: 'a'
+print(mylist.index("c"))
+
+
+mylist[1] = 'x'
+print(mylist)  # Output: ['a', 'x', 'c', 'd', 'e']
