e81268522a5b02480ccdab8b95f9be2b3e360378
ffdrko <faisal.deepto@gmail.com>
2026-04-16 12:19:47 +0600

    Day-5 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_5/Code Experimenr/cd_1.py
@@ -0,0 +1,24 @@
+# Experiment - 2 Len()
+todo_list = []
+
+while True:
+    user_Action = input("Type add, show, edit, complete or exit: ")
+
+    match user_Action:
+        case "add":
+            todo_item = input("Enter a todo item: ")
+            todo_list.append(todo_item)
+        case "show":
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}-{item}")
+                print(len(todo_list))
+        case "edit":
+            item_num = int(input("Enter the number of the item to edit: "))
+            item_num -= 1
+            todo_list[item_num] = input("Enter the new todo item: ")
+        case "complete":
+            item_num = int(input("Enter the number of the item to complete: "))
+            item_num -= 1
+            todo_list.pop(item_num)
+        case "exit":
+            break
