71ca23979e4494e7f98e901daea25d9c8ae691a0
ffdrko <faisal.deepto@gmail.com>
2026-04-16 11:00:16 +0600

    Day-5 implement the f-string for better display output

---
+++ 4_Apr_26/Part-2/Day_5/pg_1.py
@@ -0,0 +1,18 @@
+todo_list = []
+
+while True:
+    user_Action = input("Type add, show, edit or exit: ")
+
+    match user_Action:
+        case "add":
+            todo_item = input("Enter a todo item: ")
+            todo_list.append(todo_item)
+        case "show":
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}-{item}")
+        case "edit":
+            item_num = int(input("Enter the number of the item to edit: "))
+            item_num -= 1
+            todo_list[item_num] = input("Enter the new todo item: ")
+        case "exit":
+            break
