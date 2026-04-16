357c486d165a7bd15fcb92a580d3dd96ef76af56
ffdrko <faisal.deepto@gmail.com>
2026-04-16 12:34:20 +0600

    Day-5 Bug fixed

---
+++ 4_Apr_26/Part-2/Day_5/Bug/bg_1.py
@@ -0,0 +1,9 @@
+# menu = ["pasta", "pizza", "salad"]
+
+# for i, j in enumerate[menu]:
+#     print(f"{i}.{j}")
+
+menu = ["pasta", "pizza", "salad"]
+
+for i, j in enumerate(menu):
+    print(f"{i}.{j}")
