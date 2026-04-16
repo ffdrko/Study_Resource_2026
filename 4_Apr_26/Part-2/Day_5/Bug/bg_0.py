cffdf280a45e289998a1257f7461f73e85c20f74
ffdrko <faisal.deepto@gmail.com>
2026-04-16 12:33:22 +0600

    Day-5 Bug fixed

---
+++ 4_Apr_26/Part-2/Day_5/Bug/bg_0.py
@@ -0,0 +1,13 @@
+# menu = ["pasta", "pizza", "salad"]
+
+# user_choice = input("Enter the index of the item: ")
+
+# message = f"You chose {menu[user_choice]}."
+# print(message)
+
+menu = ["pasta", "pizza", "salad"]
+
+user_choice = int(input("Enter the index of the item: "))
+
+message = f"You chose {menu[user_choice]}."
+print(message)
