3daee7e43739d11a1313b051ac4abe0944706020
ffdrko <faisal.deepto@gmail.com>
2026-04-17 11:30:00 +0600

    Day-6 implement to readlines to read the list from the file

--- 4_Apr_26/Part-2/Day_6/pg_0.py
+++ 4_Apr_26/Part-2/Day_6/pg_0.py
@@ -1,15 +1,19 @@
-todo_list = []
-
 while True:
     user_action = input("Enter add, show, edit, complete or exit: ")
 
     match user_action:
         case "add":
+            file = open("File/todo_list.txt", "r")
+            todo_list =file.readlines()
+
             todo_item = input("Enter a todo item: ") + "\n"
             todo_list.append(todo_item)
             file = open("File/todo_list.txt", "w")
             file.writelines(todo_list)
         case 'show':
+            file = open("File/todo_list.txt", "r")
+            todo_list =file.readlines()
+
             for index, item in enumerate(todo_list):
                 print(f"{index + 1}. {item}")
         case 'edit':
