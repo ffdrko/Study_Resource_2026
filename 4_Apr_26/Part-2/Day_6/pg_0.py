3e831feb9b163ce337eb9a8b6e43f86af85e03ab
ffdrko <faisal.deepto@gmail.com>
2026-04-17 11:22:17 +0600

    Day-6 Apply text file to store the todo item

---
+++ 4_Apr_26/Part-2/Day_6/pg_0.py
@@ -0,0 +1,24 @@
+todo_list = []
+
+while True:
+    user_action = input("Enter add, show, edit, complete or exit: ")
+
+    match user_action:
+        case "add":
+            todo_item = input("Enter a todo item: ")
+            todo_list.append(todo_item)
+            file = open("File\todo_list.txt", "w")
+            file.writelines(todo_list)
+        case 'show':
+            for index, item in enumerate(todo_list):
+                print(f"{index + 1}. {item}")
+        case 'edit':
+            item_num = int(input("Enter the number of the item to edit: "))
+            item_num -= 1
+            todo_list[item_num] = input("Enter the new todo item: ")
+        case 'complete':
+            item_num = int(input("Enter the number of the item to edit: "))
+            item_num -= 1
+            todo_list.pop(item_num)
+        case 'exit':
+            break
