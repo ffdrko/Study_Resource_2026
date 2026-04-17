009888466584c5db7290f26607189c6e0f6225a0
ffdrko <faisal.deepto@gmail.com>
2026-04-17 12:22:58 +0600

    Day-6 bug fixed

---
+++ 4_Apr_26/Part-2/Day_6/Bug/bg_0.py
@@ -0,0 +1,14 @@
+# file = open("data.txt", 'w')
+
+# file.write("100.12")
+# file.write("111.23")
+
+# file.close()
+
+
+file = open("File/data.txt", 'w')
+
+file.write("100.12\n")
+file.write("111.23\n")
+
+file.close()
