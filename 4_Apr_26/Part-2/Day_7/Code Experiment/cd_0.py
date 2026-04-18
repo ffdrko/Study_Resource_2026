bb924616cbbfa3420423a0c9541b808779e930c1
ffdrko <faisal.deepto@gmail.com>
2026-04-18 11:25:30 +0600

    Day_7 Code Experiment

---
+++ 4_Apr_26/Part-2/Day_7/Code Experiment/cd_0.py
@@ -0,0 +1,36 @@
+while True:
+    user_action = input("Enter add, show, edit, complete or exit: ")
+
+    match user_action:
+        case "add":
+            file = open("D:\FFD WORK\FFD\Study_Resource_2026/4_Apr_26\Part-2\Day_7\File/todos.txt", "r")
+            todo_list = file.readlines()
+            file.close()
+
+            todo_item = input("Enter a todo item: ") + "\n"
+            todo_list.append(todo_item)
+
+            file = open("D:\FFD WORK\FFD\Study_Resource_2026/4_Apr_26\Part-2\Day_7\File/todos.txt", "w")
+            file.writelines(todo_list)
+            file.close()
+        case "show":
+            file = open("D:\FFD WORK\FFD\Study_Resource_2026/4_Apr_26\Part-2\Day_7\File/todos.txt", "r")
+            todo_list = file.readlines()
+            file.close()
+
+            # new_todo_list = [item.strip('\n') for item in todo_list]
+           
+            for index, item in enumerate(todo_list):
+                item = item.strip('\n')
+                print(f"{index + 1}. {item}")
+        case "edit":
+            item_no = int(input("Enter the number of the item you want to edit: "))
+            item_no -= 1
+            todo_list[item_no] = input("Enter the new todo item: ") + "\n"
+
+        case "complete":
+            item_no = int(input("Enter the number of the item you want to edit: "))
+            item_no -= 1
+            todo_list.pop(item_no)
+        case "exit":
+            break
