db0258a9fbec36f9e023b1f38e3739682c3e478c
ffdrko <faisal.deepto@gmail.com>
2026-04-19 12:05:36 +0600

    Day_8 code experiment

---
+++ 4_Apr_26/Part-2/Day_8/Code Experiment/cd_3.py
@@ -0,0 +1,6 @@
+# Experiment -4 store the file.read into a variable
+
+with open("E:\Study resource\git material/4_Apr_26\Part-2\Day_8\File\doc.txt", "r") as file:
+    content = file.read()
+
+print(content)
