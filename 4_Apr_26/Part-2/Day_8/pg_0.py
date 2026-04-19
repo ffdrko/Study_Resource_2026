33fa5e79208c211e2389f4efe44466efd01f0efd
ffdrko <faisal.deepto@gmail.com>
2026-04-19 11:51:56 +0600

    Day_8 optimize the code using with syntax in open file system

---
+++ 4_Apr_26/Part-2/Day_8/pg_0.py
@@ -0,0 +1,49 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+
+    match user_action:
+        case 'add':
+            todo_item = input("Enter a todo item: ") + "\n"
+
+            with open("File/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+            
+            todo_list.append(todo_item)
+
+            with open("File/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        
+        case 'show':
+            with open("File/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+
+            for index, item in enumerate(todo_list):
+                item = item.strip("\n")
+                print(f"{index + 1}-{item}")
+        
+        case 'edit':
+            with open("File/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+
+            item_num = int(input("Enter the number of todo item: "))
+            item_num -= 1
+
+            todo_list[item_num] = input("Enter the new todo: ") + "\n"
+
+            with open("File/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        
+        case 'complete':
+            with open("File/todo_list.txt", "r") as file:
+                todo_list = file.readlines()
+                
+            item_num = int(input("Enter the number of todo item: "))
+            item_num -= 1
+
+            todo_list.pop(item_num)
+
+            with open("File/todo_list.txt", "w") as file:
+                file.writelines(todo_list)
+        
+        case 'exit':
+            break
