49b13952f1c88e5b20d74af7e9ec723cc177f34a
ffdrko <faisal.deepto@gmail.com>
2026-04-19 12:20:31 +0600

    Day_8 Bug Fixed

---
+++ 4_Apr_26/Part-2/Day_8/Bug/bg_1.py
@@ -0,0 +1,8 @@
+# with open("file.txt", 'r') as file:
+#     print(file.read())
+#     print(len(file.read()))
+
+with open("File/file.txt", 'r') as file:
+    content = file.read()
+    print(content)
+    print(len(content))
