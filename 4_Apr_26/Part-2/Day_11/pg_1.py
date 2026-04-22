35c1fba1632699a51fe2c030d5a484fe53592c29
ffdrko <faisal.deepto@gmail.com>
2026-04-22 11:23:31 +0600

    Day_11 local and global variable

--- 4_Apr_26/Part-2/Day_11/pg_1.py
+++ 4_Apr_26/Part-2/Day_11/pg_1.py
@@ -6,3 +6,5 @@ def greet():
 
 greeting = greet()
 print(greeting)
+
+# print(new_message) will not work because new_message is a local variable inside the greet function and cannot be accessed outside of it.
