8d87c4c0d734c94d3e65c83bc707d32cd7ab7b99
ffdrko <faisal.deepto@gmail.com>
2026-04-22 11:54:49 +0600

    Day_11 Bug Fixed

---
+++ 4_Apr_26/Part-2/Day_11/Bug/bg_1.py
@@ -0,0 +1,20 @@
+# def get_maximum():
+#     celsius = [14, 15.1, 12.3]
+#     maximum = max(celsius)
+#     print(maximum)
+    
+# celsius = get_maximum()
+# fahrenheit = celsius * 1.8 + 32
+
+# print(fahrenheit)
+
+
+def get_maximum():
+    celsius = [14, 15.1, 12.3]
+    maximum = max(celsius)
+    return maximum
+    
+celsius = get_maximum()
+fahrenheit = celsius * 1.8 + 32
+
+print(fahrenheit)
