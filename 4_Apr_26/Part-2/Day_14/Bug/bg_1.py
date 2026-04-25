bca26204cd0dce24491b70209873347a8f30117f
ffdrko <faisal.deepto@gmail.com>
2026-04-25 12:18:26 +0600

    Day_14 Bug fixed

---
+++ 4_Apr_26/Part-2/Day_14/Bug/bg_1.py
@@ -0,0 +1,10 @@
+# import functions.py
+
+# nr_of_periods = functions.count("Trees are good. Grass is green.")
+# print(nr_of_periods)
+
+
+import functions
+
+nr_of_periods = functions.count("Trees are good. Grass is green.")
+print(nr_of_periods)
