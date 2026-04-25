c527f6a58d1627ebd86e1bf4c2005b297f578678
ffdrko <faisal.deepto@gmail.com>
2026-04-25 11:13:50 +0600

    Day_14 directly importing the function

--- 4_Apr_26/Part-2/Day_14/pg_0.py
+++ 4_Apr_26/Part-2/Day_14/pg_0.py
@@ -1,4 +1,5 @@
-from func_0 import get_todo, write_todo
+# from func_0 import get_todo, write_todo
+import func_0
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -6,30 +7,30 @@ while True:
 
     if user_action.startswith("add"):
         todo_item = user_action[4:] + "\n"
-        todo_list = get_todo()
+        todo_list = func_0.get_todo()
         todo_list.append(todo_item)
-        write_todo(todo_list)
+        func_0.write_todo(todo_list)
     elif user_action.startswith("show"):
-        todo_list = get_todo()
+        todo_list = func_0. get_todo()
         for index, item in enumerate(todo_list):
             item = item.strip("\n")
             print(f"{index + 1}. {item}")
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5:])
-            todo_list = get_todo()
+            todo_list = func_0.get_todo()
             new_todo = input("Enter the new todo item: ") + "\n"
             todo_list[number - 1] = new_todo
-            write_todo(todo_list)
+            func_0.write_todo(todo_list)
         except ValueError:
             print("Your command is not valid.")
             continue
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todo_list = get_todo()
+            todo_list = func_0.get_todo()
             todo_list.pop(number - 1)
-            write_todo(todo_list)
+            func_0.write_todo(todo_list)
         except ValueError:
             print("Your command is not valid.")
             continue
