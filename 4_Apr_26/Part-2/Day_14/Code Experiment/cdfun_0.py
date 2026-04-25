96679f9b104cf61ae92a50c71c35332158b66937
ffdrko <faisal.deepto@gmail.com>
2026-04-25 11:50:27 +0600

    Day_14 code experiment

--- 4_Apr_26/Part-2/Day_14/Code Experiment/cdfun_0.py
+++ 4_Apr_26/Part-2/Day_14/Code Experiment/cdfun_0.py
@@ -18,6 +18,7 @@ def write_todo(todo_list_local, file_path = "File/todo_item.txt"):
 
 
 print("This is a module for handling todo items.")
+print(x)
 # This block will only execute if this script is run directly, and not when imported as a module.
 if __name__ == "__main__":
     print("This is a module for handling todo items.")
