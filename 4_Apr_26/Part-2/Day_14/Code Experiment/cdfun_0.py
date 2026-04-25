23fce3c377c2ee2e4ca9efa785e2152919d0e018
ffdrko <faisal.deepto@gmail.com>
2026-04-25 11:49:42 +0600

    Day_14 code experiment

---
+++ 4_Apr_26/Part-2/Day_14/Code Experiment/cdfun_0.py
@@ -0,0 +1,23 @@
+def get_todo(file_path = "File/todo_item.txt"):
+    """
+    Retrieve the list of todo items from a file.
+    
+    """
+    with open(file_path) as file:
+        todo_list_local = file.readlines()
+    return todo_list_local
+
+
+def write_todo(todo_list_local, file_path = "File/todo_item.txt"):
+    """
+    Write the list of todo items to a file.
+    
+    """
+    with open(file_path, "w") as file:
+        file.writelines(todo_list_local)
+
+
+print("This is a module for handling todo items.")
+# This block will only execute if this script is run directly, and not when imported as a module.
+if __name__ == "__main__":
+    print("This is a module for handling todo items.")
