3359ae55fe4f5f51d4cb35454b7a10aaf92f8125
ffdrko <faisal.deepto@gmail.com>
2026-04-25 11:12:27 +0600

    Day_14 calling functions from the func file and import the get_todo and write_todo

--- 4_Apr_26/Part-2/Day_14/pg_0.py
+++ 4_Apr_26/Part-2/Day_14/pg_0.py
@@ -1,21 +1,4 @@
-def get_todo(file_path = "File/todo_item.txt"):
-    """
-    Retrieve the list of todo items from a file.
-    
-    """
-    with open(file_path) as file:
-        todo_list_local = file.readlines()
-    return todo_list_local
-
-
-def write_todo(todo_list_local, file_path = "File/todo_item.txt"):
-    """
-    Write the list of todo items to a file.
-    
-    """
-    with open(file_path, "w") as file:
-        file.writelines(todo_list_local)
-
+from func_0 import get_todo, write_todo
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
