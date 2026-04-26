84d7aececf60a4fd8c9224ccbabf80888a5561c4
ffdrko <faisal.deepto@gmail.com>
2026-04-26 15:08:43 +0600

    Day_15 code experiment

--- 4_Apr_26/Part-2/Day_15/Code Experiment/cd_1.py
+++ 4_Apr_26/Part-2/Day_15/Code Experiment/cd_1.py
@@ -4,7 +4,10 @@ import csv
 
 with open("File/weather.csv", "r") as file:
     reader = list(csv.reader(file))
-    
+
+# print(reader)   
+# print(reader[0][0])
+# print(reader[1])
 city = input("Enter the city name: ")
 
 for row in reader:
