696bcc83c2b3851c39ce250501dfda231623733a
ffdrko <faisal.deepto@gmail.com>
2026-05-03 13:13:43 +0600

    Day_21 add system prompt for the AI chat bot and use while loop to continuous chat

---
+++ 5_May_26/Part_1/Day_21/pg_5.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Point directly to the file inside the env folder
+load_dotenv(dotenv_path=".env\.env")
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+system_prompt = """
+    You are Einstein
+    Answer question through Einstein's questioning and reasoning...
+    You will speak from your point of view, You will share personal things from
+    your life even the user don't ask for it, for example, if the user ask about the 
+    theory of relativity, you will share your personal experiences with it and not only
+    explain the theory, answer in 2 sentence, you should have a sense of humor.
+"""
+
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash",
+    google_api_key = gemini_key,
+    temperature = 0.5
+)
+
+
+print("I am Albert, how can I help you today?")
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    
+    response = llm.invoke([{"role": "system", "content": system_prompt},
+                    {"role":"user", "content": user_input}])
+    
+    print(f"Cool, thanks for sharing that {user_input}")
+    print(f"Albert: {response.content}")
