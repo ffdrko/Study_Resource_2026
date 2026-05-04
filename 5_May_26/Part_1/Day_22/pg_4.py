66aa8d23dadafb8d85aa2af39887cc45c1fc1ada
ffdrko <faisal.deepto@gmail.com>
2026-05-04 13:20:01 +0600

    Day_22 creating multi lines in a page pdf files

---
+++ 5_May_26/Part_1/Day_22/pg_4.py
@@ -0,0 +1,35 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="p", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topic.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B",size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt = row['Topic'], align="L")
+    
+    for i in range(20, 298, 10):
+        pdf.line(10,i,200,i)
+    
+    
+    
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=24)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt = row['Topic'], align="R")
+
+    for item in range(row['Pages'] - 1):
+        pdf.add_page()
+        pdf.ln(277)
+        pdf.set_font(family="Times",style="I",size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt = row['Topic'], align="R")
+
+        for i in range(20, 298, 10):
+            pdf.line(10,i,200,i)
+    
+pdf.output("output.pdf")
