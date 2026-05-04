6d1a0f90ea2083b50a857bbe7af3d39346d8e34e
ffdrko <faisal.deepto@gmail.com>
2026-05-04 12:04:58 +0600

    Day_22 Create the multi pages pdf file

--- 5_May_26/Part_1/Day_22/pg_1.py
+++ 5_May_26/Part_1/Day_22/pg_1.py
@@ -1,3 +1,14 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="p", unit="mm", format="A4")
+
+df = pd.read_csv("topic.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=0, h=10, txt = "Hello there!", align="L")
+
+
+pdf.output("demo_3.pdf")
