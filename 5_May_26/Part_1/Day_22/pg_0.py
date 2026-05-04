17af70d847c0574c2adc804e81474831cf2f818d
ffdrko <faisal.deepto@gmail.com>
2026-05-04 11:07:05 +0600

    Day_22 Create the pdf file manually and added another line

--- 5_May_26/Part_1/Day_22/pg_0.py
+++ 5_May_26/Part_1/Day_22/pg_0.py
@@ -6,4 +6,7 @@ pdf.add_page()
 pdf.set_font(family="Times", style="B", size=12)
 pdf.cell(w= 0, h=12, txt = "hello there!", align= 'L', ln=1, border=1)
 
-pdf.output("demo.pdf")
+pdf.set_font(family="Times", style="B", size=10)
+pdf.cell(w= 5, h=12, txt = "I am Fahim!", align= 'L', ln=1, border=1)
+
+pdf.output("demo_1.pdf")
