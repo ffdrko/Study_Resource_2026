b28a863d7ee5a6d89d3c10dacd5205ff6953c396
ffdrko <faisal.deepto@gmail.com>
2026-05-04 12:28:10 +0600

    Day_22 Create the multi pages pdf file automatically

--- 5_May_26/Part_1/Day_22/pg_1.py
+++ 5_May_26/Part_1/Day_22/pg_1.py
@@ -7,8 +7,10 @@ df = pd.read_csv("topic.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font(family="Times", size=10)
-    pdf.cell(w=0, h=10, txt = "Hello there!", align="L")
+    pdf.set_font(family="Times",style="B",size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt = row['Topic'], align="L")
+    pdf.line(10,22,200,22)
 
 
-pdf.output("demo_3.pdf")
+pdf.output("demo_5.pdf")
