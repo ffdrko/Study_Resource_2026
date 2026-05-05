3d01d4e93d98d9928dace243766ced26a4e47061
ffdrko <faisal.deepto@gmail.com>
2026-05-05 12:11:11 +0600

    Day_23 single pdf file create from the multiiple txt files

--- 5_May_26/Part_1/Day_23/Practice/main.py
+++ 5_May_26/Part_1/Day_23/Practice/main.py
@@ -3,3 +3,17 @@ import glob
 from pathlib import Path
 
 filepaths = glob.glob("Practice/*.txt")
+
+pdf = FPDF(orientation="P", unit= "mm", format="A4")
+
+for file_destination in filepaths:
+    pdf.add_page()
+
+    filename = Path(file_destination).stem
+    name = filename.title()
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt = name, ln=1)
+
+
+pdf.output("Practice/animal.pdf")
