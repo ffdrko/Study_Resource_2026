0bde1b54a23e2b3f6d022f8bdf9485a8854d6302
ffdrko <faisal.deepto@gmail.com>
2026-05-07 14:31:39 +0600

    Grade Calculator display grades

--- 5_May_26/Mini Projects/Grade Calculator/main.py
+++ 5_May_26/Mini Projects/Grade Calculator/main.py
@@ -1,3 +1,20 @@
 # Store multiple subject score
 scores = [85, 92, 78, 64, 99]
-print(scores)
+# print(scores)
+
+# Display the grades
+for score in scores:
+    if score >= 90:
+        grade = "A"
+    elif score >= 80:
+        grade = "B"
+    elif score >= 70:
+        grade = "C"
+    elif score >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+    print(f"Score: {score} → Grade: {grade}")
+
+average = sum(scores) / len(scores)
+
