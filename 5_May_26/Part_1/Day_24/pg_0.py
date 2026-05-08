f9314d832ce7d1080256105a2a0ee1a53da92196
ffdrko <faisal.deepto@gmail.com>
2026-05-08 11:26:29 +0600

    Day_24 ceate pdf from execl

---
+++ 5_May_26/Part_1/Day_24/pg_0.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+import openpyxl
+
+filepath = glob.glob("Invoice/*.xlsx")
+
+for file_ded in filepath:
+    df = pd.read_excel(file_ded, sheet_name= "Sheet 1")
+    pdf = FPDF(orientation="P", unit= "mm", format= "A4")
+    pdf.add_page()
+    filename = Path(file_ded).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", size= 16, style= "B")
+    pdf.cell(w=50, h=8, txt= f"Invoice name:{invoice_nr}")
+    pdf.output(f"Pdf/{invoice_nr}.pdf")
