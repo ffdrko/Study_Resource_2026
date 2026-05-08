2020715da1c840607862ca6251f61e9f8bd49ddd
ffdrko <faisal.deepto@gmail.com>
2026-05-08 11:48:02 +0600

    Day_24 printing all cell fromt the xlsx files

--- 5_May_26/Part_1/Day_24/pg_1.py
+++ 5_May_26/Part_1/Day_24/pg_1.py
@@ -17,7 +17,16 @@ for file_ded in filepath:
 
     time_Date = filename.split("-")[1]
     pdf.set_font(family="Times", size= 16, style= "B")
-    pdf.cell(w=50, h=8, txt= f"Date:{time_Date}")
+    pdf.cell(w=50, h=8, txt= f"Date:{time_Date}", ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size = 10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
 
     pdf.output(f"Pdf/{invoice_nr}.pdf")
