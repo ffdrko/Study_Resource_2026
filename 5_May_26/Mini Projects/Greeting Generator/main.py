b6b48d0350683877e633935f58bbc1fbe3032a2f
ffdrko <faisal.deepto@gmail.com>
2026-05-09 23:43:10 +0600

    Greeting generator

---
+++ 5_May_26/Mini Projects/Greeting Generator/main.py
@@ -0,0 +1,8 @@
+def greeting(name, Age):
+    print(f"Hello, {name} and you are {Age} years old")
+
+
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+
+greeting(user_name, user_age)
