4fa2ae90f96a5acd754a0c2823aaa4392232def1
ffdrko <faisal.deepto@gmail.com>
2026-05-11 09:56:58 +0600

    Without use of type hint

---
+++ 5_May_26/Part_2/Chapter_1/main.py
@@ -0,0 +1,15 @@
+# Variable is containers for data
+
+user_name = "Alice"
+message_count = 42
+reponse_time = 0.357
+is_premium_user = True
+
+print(user_name)
+print(message_count)
+print(reponse_time)
+print(is_premium_user)
+
+""" Here in the given program there is no type mention, to find the data type 
+we need use type() manually.
+to solve these problem we need to use the type hint"""
