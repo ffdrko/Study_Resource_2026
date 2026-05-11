23dc037d466b520eb8fc4dacf7c542da5cdc6f97
ffdrko <faisal.deepto@gmail.com>
2026-05-11 09:57:23 +0600

    with using the type hint

--- 5_May_26/Part_2/Chapter_1/main.py
+++ 5_May_26/Part_2/Chapter_1/main.py
@@ -13,3 +13,13 @@ print(is_premium_user)
 """ Here in the given program there is no type mention, to find the data type 
 we need use type() manually.
 to solve these problem we need to use the type hint"""
+
+user_name: str = "Fahim"
+age: int = 20
+height_m: float = 1.75
+coded_before: bool = True 
+
+print(user_name)
+print(age)
+print(height_m)
+print(coded_before)
