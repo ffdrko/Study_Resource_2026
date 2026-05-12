db80b68f1e9f1dde716c2b32637fa78858d4e594
ffdrko <faisal.deepto@gmail.com>
2026-05-12 14:49:48 +0600

    problem practice

---
+++ 5_May_26/Part_2/Practice/pr_3.py
@@ -0,0 +1,8 @@
+#  Use a for loop to print each number
+#  from 1 to 10 and whether it's even or odd.
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
