123453cb4a1849254e017471b618a60741d5a15a
ffdrko <faisal.deepto@gmail.com>
2026-05-17 12:52:27 +0600

    use of esacape sequence

--- 5_May_26/Part_3/pg_1.py
+++ 5_May_26/Part_3/pg_1.py
@@ -4,3 +4,4 @@ print("Hey users welcome to the website\nlet's start our work")
 Here we are using the multi line comment
 no need to use # every line
 '''
+print("Hello, I am \"Deepto\" and \nhow are you.")
