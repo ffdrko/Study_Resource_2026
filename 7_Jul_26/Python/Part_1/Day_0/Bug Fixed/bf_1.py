4b0a04002d825e26b0b5495d98ef36049f6f3990
ffdrko <faisal.deepto@gmail.com>
2026-07-04 12:50:23 +0600

    Day 0 bug fixed two done.

---
+++ 7_Jul_26/Python/Part_1/Day_0/Bug Fixed/bf_1.py
@@ -0,0 +1,7 @@
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+#
+# SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No', my_answer]
