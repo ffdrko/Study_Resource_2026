dd0d5ecb41ba99ff92decba00a222df9029dc576
ffdrko <faisal.deepto@gmail.com>
2026-07-04 12:53:32 +0600

    Day 0 bug fixed four done.

---
+++ 7_Jul_26/Python/Part_1/Day_0/Bug Fixed/bf_3.py
@@ -0,0 +1,7 @@
+# my_answer = input["What is your answer?"]
+# answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+#
+# TypeError: 'builtin_function_or_method' object is not subscriptable
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
