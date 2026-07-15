a638b29146cd82576f15d90b093c902207f89694
ffdrko <faisal.deepto@gmail.com>
2026-07-15 20:17:57 +0600

    Multti line quotes

--- 7_Jul_26/Python/Part_2/string.py
+++ 7_Jul_26/Python/Part_2/string.py
@@ -1,7 +1,12 @@
 a = 'this is a string'
 b = "this is also a string"
+c = """This is a multi line string.
+it is inside multi line quotes.
+"""
 
 print(a)
 print(b)
+print(c)
 print(type(a))
 print(type(b))
+print(type(c))
