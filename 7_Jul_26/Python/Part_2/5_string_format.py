1b26923cc95efaef630c312d0d2c9f254698fd71
ffdrko <faisal.deepto@gmail.com>
2026-07-16 22:40:31 +0600

    string format long cut

---
+++ 7_Jul_26/Python/Part_2/5_string_format.py
@@ -0,0 +1,14 @@
+# user_input = input("Enter your name: ")
+
+# a = "Good Morning, {}. How are you?".format(user_input)
+
+
+# print(user_input)
+# print(a)
+
+age = 20
+f_name = "Rahim"
+l_name = "Khan"
+
+txt = "My name is {first} {last} and I am {age} years old.".format(first=f_name, last=l_name, age=age)
+print(txt)
