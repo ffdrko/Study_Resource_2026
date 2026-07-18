186b45d49c6c24a7947826ffb39d6c279d1cc15c
ffdrko <faisal.deepto@gmail.com>
2026-07-18 11:10:05 +0600

    number precedence

---
+++ 7_Jul_26/Python/Part_2/6_operator.py
@@ -0,0 +1,16 @@
+a = 4 + 5
+b = 4 - 5
+c = 4 * 5
+d = 10 / 5 # float division
+e = 10 // 5 # integer division
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+x = (12+34) - 45 + 78 / 2 + 12*2
+
+
+print(x)
