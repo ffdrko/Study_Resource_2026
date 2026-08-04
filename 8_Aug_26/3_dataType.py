647c36e058ce1210f3c4657eeaec683ffbb39e59
ffdrko <faisal.deepto@gmail.com>
2026-08-04 20:52:45 +0600

    Datatype

---
+++ 8_Aug_26/3_dataType.py
@@ -0,0 +1,19 @@
+name = "Fahim"
+
+print(name)
+print(type(name))
+
+roll = 10
+
+print(roll)
+print(type(roll))
+
+pi = 3.14
+
+print(pi)
+print(type(pi))
+
+is_rain = True
+
+print(is_rain)
+print(type(is_rain))
