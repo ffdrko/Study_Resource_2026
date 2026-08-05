0a567c6c1a72ad77465e0325021d41363aa77e90
ffdrko <faisal.deepto@gmail.com>
2026-08-05 11:54:31 +0600

    input function

---
+++ 8_Aug_26/8_input.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name: ")
+
+print("Hello, " + user_name + "! Welcome to the program.")
+
+num = int(input("Enter a number: "))
+print("You entered:", num)
+print(type(num))
