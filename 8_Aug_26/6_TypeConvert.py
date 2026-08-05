71b8ee57c98eb1396f334393319f9375c2430eda
ffdrko <faisal.deepto@gmail.com>
2026-08-05 10:17:07 +0600

    Type conversation.

---
+++ 8_Aug_26/6_TypeConvert.py
@@ -0,0 +1,19 @@
+num = '10'
+print(num)
+print(type(num))
+
+ConNum = int(num)
+print(ConNum)
+print(type(ConNum))
+
+pi = 3.14
+print(pi)
+print(type(pi))
+
+ConPi_1 = str(pi)
+print(ConPi_1)    
+print(type(ConPi_1))
+
+ConPi_12 = int(pi)
+print(ConPi_12)    
+print(type(ConPi_12))
