dd2dba2916951160ea2cfdd8df1e9ff2ad2311cc
ffdrko <faisal.deepto@gmail.com>
2026-08-05 12:04:05 +0600

    simple calculator

---
+++ 8_Aug_26/9_calculator.py
@@ -0,0 +1,10 @@
+# Simple Calculator
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+print("=" * 30)
+print("Addition of", num_1, "and", num_2, "is:", num_1 + num_2)
+print("Subtraction of", num_1, "and", num_2, "is:", num_1 - num_2)
+print("Multiplication of", num_1, "and", num_2, "is:", num_1 * num_2)
+print("Division of", num_1, "and", num_2, "is:", num_1 / num_2)
