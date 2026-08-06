027202ab9220356d8ace8d3b12abb1123fbc33a5
ffdrko <faisal.deepto@gmail.com>
2026-08-06 19:53:57 +0600

    Question_2 Done

---
+++ 8_Aug_26/Assignment_1/Question_2.py
@@ -0,0 +1,7 @@
+# Asking user for their name, city, and favorite hobby
+user_name = input("Enter your name: ")
+user_city = input("Enter your city: ")
+user_hobby = input("Enter your favorite hobby: ")
+
+# printing the introduction sentence.
+print(f"Hello {user_name}! You live in {user_city} and you enjoy {user_hobby}.")
