885e406fb876c16659c1fc59700f384eb25f3e4b
ffdrko <faisal.deepto@gmail.com>
2026-08-06 19:45:28 +0600

    Question_1 Done

---
+++ 8_Aug_26/Assignment_1/Question_1.py
@@ -0,0 +1,9 @@
+# Asking user for to input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculation and printing the result
+print(f"The sum of {num1} and {num2} is: {num1 + num2}") # Addition
+print(f"The difference of {num1} and {num2} is: {num1 - num2}") # Subtraction
+print(f"The product of {num1} and {num2} is: {num1 * num2}") # Multiplication
+print(f"The quotient of {num1} and {num2} is: {num1 / num2}") # Division
