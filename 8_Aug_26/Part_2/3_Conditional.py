1beac86e79abce4a898f53e8b6abe6d1228dbee2
ffdrko <faisal.deepto@gmail.com>
2026-08-08 11:32:21 +0600

    conditional

---
+++ 8_Aug_26/Part_2/3_Conditional.py
@@ -0,0 +1,6 @@
+age = int(input("Please enter your age: "))
+
+if age >= 18:
+    print("You are an adult and eligible to apply for a driving license.")
+else:
+    print("You are not an adult and not eligible to apply for a driving license.")
