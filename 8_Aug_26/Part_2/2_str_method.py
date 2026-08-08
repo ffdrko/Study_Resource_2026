95d6d57c2caf2603485d665b2e47969e41522106
ffdrko <faisal.deepto@gmail.com>
2026-08-08 11:13:41 +0600

    string method

---
+++ 8_Aug_26/Part_2/2_str_method.py
@@ -0,0 +1,12 @@
+course_name = "  oStad ai ##booTcamp  "
+print(course_name)
+
+course_name = course_name.strip()
+print(course_name)
+
+course_name = course_name.lower()
+print(course_name)
+course_name = course_name.replace("##", "")
+print(course_name)
+course_name = course_name.title()
+print(course_name)
