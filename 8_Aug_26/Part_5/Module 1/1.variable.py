bfed39f424c3a40e06239bc1c8e83bad2f8d8a80
ffdrko <faisal.deepto@gmail.com>
2026-08-26 13:00:49 +0600

    Add initial variable operations and print statements

---
+++ 8_Aug_26/Part_5/Module 1/1.variable.py
@@ -0,0 +1,24 @@
+print(30 + 20)
+print(30 - 20)
+print(30 * 20)
+print(30 / 20)
+
+
+print(60 + 40)
+print(60 - 40)
+print(60 * 40)
+print(60 / 40)
+
+a = 10
+b = 30
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+name = "Fahim Faisal Deepto"
+print(name)
+
+country = "Bangladesh"
+print(country)
