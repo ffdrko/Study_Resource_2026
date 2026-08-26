d2d1f91bc762d7217acd68442c2a482baf29d180
ffdrko <faisal.deepto@gmail.com>
2026-08-26 13:18:38 +0600

    Add datatype examples and type checks in 3.datatype.py

---
+++ 8_Aug_26/Part_5/Module 1/3.datatype.py
@@ -0,0 +1,12 @@
+name = "Fahim Faisal Deepto" # text -> string -> str
+roll = 10 # number -> integer -> int
+pi = 3.1416 # number -> floating point number -> float
+isRain = True # boolean -> bool
+
+print(type(name))
+print(type(roll))
+print(type(pi))
+print(type(isRain))
+
+print(type("3.1416"))
+print(type(3.1416))
