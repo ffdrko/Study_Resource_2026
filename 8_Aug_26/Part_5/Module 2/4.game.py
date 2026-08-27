a18ba970ffbe681a8f0d42adc834d2f7ea632044
ffdrko <faisal.deepto@gmail.com>
2026-08-27 13:56:09 +0600

    guess game

---
+++ 8_Aug_26/Part_5/Module 2/4.game.py
@@ -0,0 +1,9 @@
+computer = 4
+
+guess = int(input("Enter your guess: "))
+
+while guess != computer:
+    print("Wrong guess, Please Try again!")
+    guess = int(input("Enter your guess: "))
+
+print("Nice u it's a match")
