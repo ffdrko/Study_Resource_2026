0a973a00fac795299b897668141bf3f3114081b7
ffdrko <faisal.deepto@gmail.com>
2026-08-27 12:58:07 +0600

    string method

---
+++ 8_Aug_26/Part_5/Module 2/0.str_met.py
@@ -0,0 +1,15 @@
+greet = " Hello WoRld "
+print(greet)
+print(greet.upper())
+print(greet.lower())
+print(greet.title())
+print(greet.strip())
+print(greet.strip().capitalize())
+print(greet.count('o'))
+print(greet.find('l'))
+print(greet.isdigit()) # check if the variable value is number
+print(greet.split())
+
+text = "Python"
+print(text.index("n"))
+print(len(text)) # for finding the lenght of the string.
