72e6b2bf1b9dfe446b2f69d67b432e24662805a6
ffdrko <faisal.deepto@gmail.com>
2026-08-30 18:32:36 +0600

    iteration list, tuple and set

---
+++ 8_Aug_26/Part_5/Module 3.2/0.lst.py
@@ -0,0 +1,16 @@
+student_list = ["Alvi", "Abir", "Akash"]
+
+for item in student_list:
+    print(item)
+
+
+student_tuple = ("Alvi", "Abir", "Akash")
+
+for item in student_tuple:
+    print(item)
+
+
+student_set = {"Alvi", "Abir", "Akash"}
+
+for item in student_set:
+    print(item)
