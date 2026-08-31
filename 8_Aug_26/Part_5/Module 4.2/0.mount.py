ebd8ae321ed2354d4a41a347ec1cb66f736628cf
ffdrko <faisal.deepto@gmail.com>
2026-08-31 21:24:31 +0600

    file creation with write function

--- 8_Aug_26/Part_5/Module 4.2/0.mount.py
+++ 8_Aug_26/Part_5/Module 4.2/0.mount.py
@@ -1,2 +1,8 @@
-from google.colab import drive
-drive.mount('/content/drive')
+# from google.colab import drive
+# drive.mount('/content/drive')
+
+file = open("File/sample.txt", "w")
+file.write("Hello World\n")
+file.write("Ostad\n")
+file.write("Mastering the python\n")
+file.close()
