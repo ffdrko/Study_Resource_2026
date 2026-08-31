898f02b94a0e920a31537dedda071a5807279553
ffdrko <faisal.deepto@gmail.com>
2026-08-31 13:19:16 +0600

    practice done

---
+++ 8_Aug_26/Part_5/Module 4.1/practice/0.pracetice.py
@@ -0,0 +1,19 @@
+"""
+Write a function greet_student(name, course) that returns a welcome message.
+```python
+name = "Shovon"
+course = "Python"
+```
+
+**Example Output 1:**
+
+```python
+"Hello Shovon, welcome to Python course!"
+```
+"""
+
+def greet_student(name, course):
+        print(f"Hello {name}, welcome to {course} course!")
+
+
+greet_student("Fahim", "physic")
