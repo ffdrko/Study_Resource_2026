43f920a32914ea1e569976f47b54932d83416cbc
ffdrko <faisal.deepto@gmail.com>
2026-09-01 09:47:25 +0600

    use of context maneger using with for file

--- 9_sep_26/Bootcamp/Module 4.2/0.mount.py
+++ 9_sep_26/Bootcamp/Module 4.2/0.mount.py
@@ -15,3 +15,14 @@ file.close()
 file = open("File/sample.txt", "a")
 file.write("new line added by append\n")
 file.close()
+
+
+with open("File/sample.txt", "w") as file:
+    file.write("This is a new content\n")
+    file.write("this is new file.\n")
+
+
+
+with open("File/sample.txt", "r") as file:
+    content = file.readlines()
+    print(content)
