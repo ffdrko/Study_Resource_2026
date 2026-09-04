b11e8d1220a4ea7b2af148363ecfc0c8914a529e
ffdrko <faisal.deepto@gmail.com>
2026-09-04 21:40:40 +0600

    use more function iniside the class

--- 9_sep_26/Bootcamp/Module 4.2/OOP/1.oop.py
+++ 9_sep_26/Bootcamp/Module 4.2/OOP/1.oop.py
@@ -1,11 +1,18 @@
 class Student:
     def __init__(self, name, marks):
-        print("adding new student")
+        # print("adding new student")
         self.name = name
         self.marks = marks
+
+    def hello(self):
+        print(f"Hello, {self.name}")
+
+    def get_marks(self):
+        return self.marks
 
 student1 = Student("Karan", 85)
 student2 = Student("Fahim", 90)
 
-print(student1.name, student1.marks)
-print(student2.name, student2.marks)
+student1.hello()
+student1_marks = student1.get_marks()
+print(student1_marks)
