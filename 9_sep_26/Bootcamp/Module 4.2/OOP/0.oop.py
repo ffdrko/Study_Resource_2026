6c995d7a766a448815faad1fcc92bc54cba80e37
ffdrko <faisal.deepto@gmail.com>
2026-09-04 21:13:57 +0600

    creating class and object

---
+++ 9_sep_26/Bootcamp/Module 4.2/OOP/0.oop.py
@@ -0,0 +1,18 @@
+# # Class is a blueprint for creating objects.
+# class Student:
+#     name = "Karan"
+
+# # Create an object of the student class
+
+# student1 = Student()
+
+# print(student1)
+# print(student1.name)
+
+class Car:
+    color = "Red"
+    brand = "Toyota"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
